e836feb5a02b5647207ad10efe5606eb03b01b00
Aurélien <atondoux@gmail.com>
2024-11-24 15:04:47 +0100

    :sparkles: add tournaments

---
+++ src/main/java/com/dyma/tennis/service/TournamentDataRetrievalException.java
@@ -0,0 +1,8 @@
+package com.dyma.tennis.service;
+
+public class TournamentDataRetrievalException extends RuntimeException {
+
+    public TournamentDataRetrievalException(Exception e) {
+        super("Could not retrieve tournament data", e);
+    }
+}
