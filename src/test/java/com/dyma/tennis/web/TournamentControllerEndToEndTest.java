4d2930702dba85cda332e7c5a3927fb7256b47dd
Aurélien <atondoux@gmail.com>
2025-12-05 09:16:34 +0100

    :arrow_up: spring boot 4 upgrade

--- src/test/java/com/dyma/tennis/web/TournamentControllerEndToEndTest.java
+++ src/test/java/com/dyma/tennis/web/TournamentControllerEndToEndTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -24,6 +25,7 @@ import java.util.UUID;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
 public class TournamentControllerEndToEndTest {
 
     @LocalServerPort
